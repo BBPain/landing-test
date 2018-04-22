d16b81e7aba4c090123253ea4c78124a5a18cf68
grzegorzbors <gbors@o2.pl>
2018-04-22 19:17:53 +0200

    changed queries

--- js/main.js
+++ js/main.js
@@ -11,10 +11,5 @@ $(document).ready(function () {
         if (width > 576 && $('#menu-list').is(':hidden')) {
             $('#menu-list').removeAttr('style');
         }
-
-/*         let docHeight = $(document).height();
-        $('.wrapper').css('height', docHeight); */
-
-        // $('footer').css('top', docHeight+'px');
     });
 });
